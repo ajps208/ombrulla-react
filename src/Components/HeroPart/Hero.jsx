66f01adc9476faf0cac0ac8cd0c79ede88885970
ajps208 <ajithps208@gmail.com>
2023-12-19 17:11:13 +0530

    image updated4

--- src/Components/HeroPart/Hero.jsx
+++ src/Components/HeroPart/Hero.jsx
@@ -48,7 +48,7 @@ function Hero() {
                 <img
                   src="https://www.ombrulla.com/_astro/ai-visual-inspection_banner_1.f3232628_ZyIKMH.webp"
                   alt="ombrulla"
-                  className="maskimg w-[55rem] h-auto md:absolute md:top-[100px] md:left-[870px]"
+                  className="maskimg w-full h-auto md:absolute md:top-[100px] md:left-[870px]"
                 />
               </div>
             </div>
