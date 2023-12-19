df8b3fb6d3039fa6a5819612ac558f3a1180cbc9
ajps208 <ajithps208@gmail.com>
2023-12-19 17:20:54 +0530

    updated img3

--- src/Components/HeroPart/Hero.jsx
+++ src/Components/HeroPart/Hero.jsx
@@ -46,10 +46,10 @@ function Hero() {
                 <div className="mt-10 border-2 md:absolute md:top-[50px] md:left-[800px] sliderShape border-blue-600 rounded-full overflow-hidden hidden md:block"></div>
                 <div className="anotherShape mt-10 border-2 md:absolute md:top-[50px] md:left-[1020px] border-white rounded-full overflow-hidden hidden md:block"></div>
                 <img
-                 width={"80rem"}
+                 width={"10rem"}
                   src="https://www.ombrulla.com/_astro/ai-visual-inspection_banner_1.f3232628_ZyIKMH.webp"
                   alt="ombrulla"
-                  className="maskimg w-full h-auto md:absolute md:top-[100px] md:left-[870px]"
+                  className="maskimg  h-auto md:absolute md:top-[100px] md:left-[870px]"
                 />
               </div>
             </div>
